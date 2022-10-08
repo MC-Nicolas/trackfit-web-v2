a2c6737e9b73d5c7a8a26d7c9ca9dd91b555f8f1
Nico <nico@nicos-macbook-pro.home>
2022-10-08 20:47:47 +0200

    updated next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
       'images.unsplash.com',
       'www.beautymuscle.net',
       'arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com',
+      'theprogrm.com',
+      'scontent-cdg2-1.cdninstagram.com',
     ],
   },
 };
