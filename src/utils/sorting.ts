57f8c5d5c9f1f093c92f0597381a3998d36f0a4c
Nico <nico@nicos-macbook-pro.home>
2022-10-08 17:13:36 +0200

    fix

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -3,9 +3,10 @@ import { formatFRDateToNewDate, formatIdToDate } from './format';
 export const sortByDate = (elementsToFormat: any[], fromId?: boolean) => {
   elementsToFormat.sort((a, b) => {
     if (fromId) {
-      console.log(new Date(formatIdToDate(a.id)).getTime());
-      console.log(new Date(formatIdToDate(b.id)).getTime());
-      return new Date(formatIdToDate(a.id)).getTime() - new Date(formatIdToDate(b.id)).getTime();
+      return (
+        new Date(formatFRDateToNewDate(formatIdToDate(a.id))).getTime() -
+        new Date(formatFRDateToNewDate(formatIdToDate(b.id))).getTime()
+      );
     } else {
       return (
         new Date(formatFRDateToNewDate(a.date)).getTime() -
